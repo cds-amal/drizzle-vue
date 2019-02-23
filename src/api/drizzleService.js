e15661dc2aa893f02fa410f24bf37ffa5730837d
Amal Sudama <cds.sudama@gmail.com>
2019-02-23 07:33:59 -0500

    wip

--- src/api/drizzleService.js
+++ src/api/drizzleService.js
@@ -1,30 +1,46 @@
 import { Observable } from 'rxjs'
-import { distinctUntilChanged } from 'rxjs/operators'
+import { distinctUntilChanged, tap } from 'rxjs/operators'
+import { isEqual } from 'lodash'
 
 import { Drizzle } from 'drizzle'
 import options from '../drizzleOptions'
 
-const toObservable = store => ({
-  subscribe({ onNext }) {
-    let dispose = store.subscribe(() => onNext(store.getState()))
-    onNext(store.getState())
+/* const toObservable = store => ({
+ *   subscribe({ onNext }) {
+ *     let dispose = store.subscribe(() => onNext(store.getState()))
+ *     onNext(store.getState())
+ *     return { dispose }
+ *   }
+ * }) */
+
+const toObservable = store => {
+  return Observable.create(subscriber => {
+    let dispose = store.subscribe(() => subscriber.next(store.getState()))
+    // subscriber.next(store.getState())
     return { dispose }
-  }
-})
+  })
+}
 
 const drizzle = new Drizzle(options)
 console.log('store', drizzle.store)
 console.log('drizzle', Object.keys(drizzle))
 console.log('drizzle', drizzle)
 
-const obs = toObservable(drizzle.store)
-console.log('obs', obs)
+const obs$ = toObservable(drizzle.store).pipe(
+  tap(x => console.log('...'.repeat(22))),
+  distinctUntilChanged(isEqual)
+)
+console.log('obs$', obs$)
 
-obs
-  .subscribe({
-  onNext: state => console.log('onNext: ', JSON.stringify(state, null, 2)),
+let dispose = obs$.subscribe({
+  // onNext: state => console.log('onNext: ', JSON.stringify(state, null, 2)),
+  next: state => console.log('onNext: ', JSON.stringify(state, null, 2)),
   error: err => console.log(`Oops... ${err}`),
-  complete: () => console.log(`Complete!`),
+  complete: () => console.log(`Complete!`)
 })
 
-export default obs
+setTimeout(() => dispose.dispose(), 5000)
+
+console.log('dispose', dispose)
+
+export default obs$
