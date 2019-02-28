7305925137e540645d4f9996976c3e5d3fb6b4b4
Amal Sudama <cds.sudama@gmail.com>
2019-02-28 10:29:10 -0500

    create drizzle vuex plugin

---
+++ src/store/DrizzleVuexPlugin.js
@@ -0,0 +1,52 @@
+import { map, distinctUntilChanged, filter } from 'rxjs/operators'
+import { isEqual } from 'lodash'
+
+const subscribe = (obs$, handler) => {
+  console.log('create subscription!')
+  obs$.subscribe({
+    next: message => handler(message),
+    error: err => console.log(`Oops... ${err}`),
+    complete: () => console.log(`Complete!`)
+  })
+}
+
+const drizzleHandler = store => {
+  let drizzleInitialized = false
+  return message => {
+    if (!drizzleInitialized) {
+      if (message.drizzleStatus.initialized) {
+        drizzleInitialized = true
+        store.dispatch('drizzle/initialize')
+        // handle cacheKey registration after drizzle is initialized
+        // The contracts that need cacheKey resolved were queued to
+        // the store at component creation time, which occurs before
+        // drizzle initialization.
+        //
+        store.dispatch('drizzle/processRegistrationQueue')
+      }
+    }
+  }
+}
+
+const contractsHandler = store => message => {
+  console.log('process Contracts:', message)
+  for (let contractName in message) {
+    store.dispatch('contracts/updateContract', {
+      contractName,
+      contract: message[contractName]
+    })
+  }
+}
+
+const createDrizzlePluginFromObserver = drizzleObserver$ => state => {
+  const contractsObserver$ = drizzleObserver$.pipe(
+    filter(x => x.drizzleStatus.initialized),
+    map(x => x.contracts),
+    distinctUntilChanged(isEqual)
+  )
+
+  const drizzleSub = subscribe(drizzleObserver$, drizzleHandler(state))
+  const contractSub = subscribe(contractsObserver$, contractsHandler(state))
+}
+
+export default createDrizzlePluginFromObserver
--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,7 @@ import Vuex from 'vuex'
 import drizzle from './modules/drizzle'
 import contracts from './modules/contracts'
 import drizzlePlugin from '@/DrizzlePlugin'
-
-import { map, distinctUntilChanged, filter } from 'rxjs/operators'
-import { isEqual } from 'lodash'
+import drizzleVuexPlugin from './DrizzleVuexPlugin'
 
 Vue.use(drizzlePlugin)
 Vue.use(Vuex)
@@ -14,65 +12,8 @@ const store = new Vuex.Store({
   modules: {
     drizzle,
     contracts
-  }
+  },
+  plugins: [drizzleVuexPlugin(Vue.drizzleObserver$)]
 })
-
-// Redux Subscription
-let reduxSubscription = Vue.drizzleObserver$.subscribe({
-  next: state => processState(state),
-  error: err => console.log(`Oops... ${err}`),
-  complete: () => console.log(`Complete!`)
-})
-
-console.log('reduxSubscription', reduxSubscription)
-
-let drizzleInitialized = false
-const processState = state => {
-  /* console.log('Redux State update', state)
-   * console.log(
-   *   'drizzleInitialized',
-   *   drizzleInitialized,
-   *   'drizleStatus',
-   *   state.drizzleStatus.initialized
-   * ) */
-  if (!drizzleInitialized) {
-    if (state.drizzleStatus.initialized) {
-      drizzleInitialized = true
-      // console.log('toggled drizzleInitialized', drizzleInitialized)
-      store.dispatch('drizzle/initialize')
-      // handle cacheKey registration
-      // console.log('about to dispatch processRegistrationQueue')
-      store.dispatch('drizzle/processRegistrationQueue')
-    }
-  }
-}
-
-const contractsObserver$ = Vue.drizzleObserver$.pipe(
-  filter(x => x.drizzleStatus.initialized),
-  map(x => x.contracts),
-  distinctUntilChanged(isEqual)
-)
-
-// Subscribe
-let contractSubscriber = contractsObserver$.subscribe({
-  next: state => processContractState(state),
-  error: err => console.log(`Oops... ${err}`),
-  complete: () => console.log(`Complete!`)
-})
-
-const processContractState = state => {
-  console.log('process Contracts:', state)
-  for (let contractName in state) {
-    /* console.log(
-     *   'DISPATCHING contracts/updateContract',
-     *   contractName,
-     *   state[contractName]
-     * ) */
-    store.dispatch('contracts/updateContract', {
-      contractName,
-      contract: state[contractName]
-    })
-  }
-}
 
 export default store
