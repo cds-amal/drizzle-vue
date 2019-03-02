f1e1e8595a5248baf4aabe71b357d7306bbacd68
Amal Sudama <cds.sudama@gmail.com>
2019-03-02 18:14:38 -0500

    wip - show more contracts/components

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,21 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <h1>Simple Storage</h1>
     <Contract contractName="SimpleStorage" method="storedData" />
+    <h1>Complex Storage</h1>
+    <Contract contractName="ComplexStorage" method="string1" toUtf8 />
+    <Contract contractName="ComplexStorage" method="storeduint1" />
+    <h1>Complex Storage object</h1>
     <Contract contractName="ComplexStorage" method="singleDD" />
-    <!-- <ContractData contractName="SimpleStorage" method="storedData" />
-       - <h1>Complex Storage</h1>
-       - <ContractData contractName="ComplexStorage" method="string1" toUtf8 />
-       - <ContractData contractName="ComplexStorage" method="storeduint1" />
-       - <h1>Complex Storage</h1>
-       - <ContractArrayData contractName="ComplexStorage" method="singleDD" /> -->
   </div>
 </template>
 
 <script>
 import Contract from './components/Contract.vue'
-//import ContractData from './components/ContractData.vue'
-//import ContractArrayData from './components/ContractArrayData.vue'
 
 export default {
   name: 'app',
   components: {
     Contract
-//    ContractData,
-//    ContractArrayData
   }
 }
 </script>
