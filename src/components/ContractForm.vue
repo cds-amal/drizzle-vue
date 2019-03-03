b4892007e5157e19d5eeb39f64405797712d8611
Amal Sudama <cds.sudama@gmail.com>
2019-03-03 12:50:22 -0500

    wip

--- src/components/ContractForm.vue
+++ src/components/ContractForm.vue
@@ -1,15 +1,47 @@
 <template>
   <div>
-    <h1>ContractForm</h1>
+    <form>
+      <input v-model="ethData" />
+      <button @click.prevent="onSubmit">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
-  export default{
-    name: "ContractForm"
+import { mapGetters, mapState } from 'vuex'
 
+export default {
+  name: 'ContractForm',
+  props: {
+    contractName: {
+      type: String,
+      required: true
+    },
+    method: {
+      type: String,
+      required: true
+    }
+  },
+
+  //computed: mapGetters('drizzle', ['getContractABI']),
+  computed: mapState(['drizzle']),
+
+  data() {
+    return {
+      ethData: '',
+      abi: null
+    }
+  },
+
+
+  methods: {
+    onSubmit() {
+      console.log('submitting', this.ethData)
+      //console.log('abi', this.getContractABI(this.contractName, this.method))
+      console.log('abi', this.abi)
+    }
   }
+}
 </script>
-<style scoped>
 
-</style>
+<style></style>
--- src/store/modules/drizzle/getters.js
+++ src/store/modules/drizzle/getters.js
@@ -1,3 +1,11 @@
 export const isDrizzleInitialized = state => state.initialized
 
 export const getRegisteredContracts = state => state.registeredContracts
+
+export const getContractABI = state  => (contract, methodName) => {
+  console.log('getContractABI...')
+  const abi = state.drizzleInstance.contracts[contract].abi
+    .find(fn => fn.name === methodName)
+  return abi
+}
+
