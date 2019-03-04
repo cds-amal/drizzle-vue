7b6d2606bc4744b1c5411c318ea2d5daeb8d6c15
Amal Sudama <cds.sudama@gmail.com>
2019-03-04 09:54:02 -0500

    wip - initial hookup

--- src/components/ContractForm.vue
+++ src/components/ContractForm.vue
@@ -1,14 +1,34 @@
 <template>
-  <div>
+  <div v-if="isDrizzleInitialized">
+    <h1>{{ ethData }}</h1>
     <form>
-      <input v-model="ethData" />
+      <input
+        v-for="(param, i) in displayInputs"
+        v-model="ethData[i]"
+        :key="i"
+        :type="param.type"
+      />
       <button @click.prevent="onSubmit">Submit</button>
     </form>
+    <div>{{ displayInputs }}</div>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapState } from 'vuex'
+import { mapGetters } from 'vuex'
+
+const translateType = type => {
+  switch (true) {
+    case /^uint/.test(type):
+      return 'number'
+    case /^string/.test(type) || /^bytes/.test(type):
+      return 'text'
+    case /^bool/.test(type):
+      return 'checkbox'
+    default:
+      return 'text'
+  }
+}
 
 export default {
   name: 'ContractForm',
@@ -24,21 +44,53 @@ export default {
   },
 
   //computed: mapGetters('drizzle', ['getContractABI']),
-  computed: mapState(['drizzle']),
+  computed: {
+    ...mapGetters('drizzle', ['drizzleInstance', 'isDrizzleInitialized']),
 
-  data() {
-    return {
-      ethData: '',
-      abi: null
+    abi() {
+      const di = this.drizzleInstance
+      return di.contracts[this.contractName].abi.find(
+        item => item.name === this.method
+      )
+    },
+
+    abiInputs() {
+      return this.abi.inputs
+    },
+
+    displayInputs() {
+      const di = this.drizzleInstance
+      return di.contracts[this.contractName].abi
+        .find(item => item.name === this.method)
+        .inputs.map(x => ({ ...x, type: translateType(x.type) }))
+    },
+
+    utils() {
+      return this.drizzleInstance.web3.utils
     }
   },
 
+  data() {
+    return {
+      ethData: {}
+    }
+  },
 
   methods: {
     onSubmit() {
       console.log('submitting', this.ethData)
-      //console.log('abi', this.getContractABI(this.contractName, this.method))
       console.log('abi', this.abi)
+      console.log('abi.inputs', this.abiInputs)
+      console.log('this.method', this.method)
+      const convertedInputs = this.abiInputs.map((input, i) =>
+        input.type === 'bytes32'
+          ? (this.ethData[i] = this.utils.toHex(this.ethData[i]))
+          : this.ethData[i]
+      )
+      console.log('convertedInputs', convertedInputs)
+      this.drizzleInstance.contracts[this.contractName].methods[
+        this.method
+      ].cacheSend(...convertedInputs)
     }
   }
 }
--- src/store/modules/drizzle/getters.js
+++ src/store/modules/drizzle/getters.js
@@ -2,10 +2,14 @@ export const isDrizzleInitialized = state => state.initialized
 
 export const getRegisteredContracts = state => state.registeredContracts
 
-export const getContractABI = state  => (contract, methodName) => {
-  console.log('getContractABI...')
-  const abi = state.drizzleInstance.contracts[contract].abi
-    .find(fn => fn.name === methodName)
-  return abi
+export const drizzleInstance = state => state.drizzleInstance
+
+
+export const getAbi = state => (contractName, method) => {
+  console.log(contractName, method)
+  console.log(state.drizzleInstance.contracts)
+  console.log(JSON.stringify(state.drizzleInstance.contracts, null, 2))
+  return state.drizzleInstance.contracts[contractName].abi//.find(abi => abi.name === method)
+
 }
 
