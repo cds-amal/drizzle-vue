ee3d194a211fa6a43b02e46e92f4c10efe2aabb3
Amal Sudama <cds.sudama@gmail.com>
2019-03-04 21:29:57 -0500

    remove comment

--- src/drizzleOptions.js
+++ src/drizzleOptions.js
@@ -11,7 +11,6 @@ const options = {
     }
   },
   contracts: [SimpleStorage,ComplexStorage, TutorialToken],
-  //contracts: [SimpleStorage, ComplexStorage],
   events: {
     SimpleStorage: ['StorageSet']
   },
